e6b2ac7ce6379e98b4b3f9efecbbf231c6fda830
andersym01 <anders.ym@hotmail.no>
2023-11-24 19:27:40 +0100

    egne js filer

---
+++ index.js
@@ -0,0 +1,55 @@
+function leggTilTilMineturer(index) {
+    var lagredeTurer = JSON.parse(localStorage.getItem('lagredeTurer')) || [];
+    var valgtTur = lagredeTurer[index];
+
+    var mineturer = JSON.parse(localStorage.getItem('mineturer')) || [];
+    mineturer.push(valgtTur);
+    localStorage.setItem('mineturer', JSON.stringify(mineturer));
+
+    alert('Turen er lagt til i Mineturer-siden.');
+
+    // Naviger til mineturer.html etter å ha lagt til turen
+    window.location.href = 'mineturer.html';
+  }
+
+  function leggTilLagredeTurer() {
+  var lagredeTurer = JSON.parse(localStorage.getItem('lagredeTurer')) || [];
+  var lagredeTurerElement = document.getElementById('lagredeTurer');
+
+  lagredeTurer.forEach(function(tur, index) {
+    var turElement = document.createElement('div');
+    turElement.classList.add('tour');
+    turElement.innerHTML = `
+      <img src="${tur.bildeUrl}" alt="${tur.tittel}">
+      <div class="tour-info">
+        <div class="tour-description">
+          <h1><a href="#">${tur.tittel}</a></h1>
+          <p>${tur.beskrivelse}</p>
+          <button class="fjern" onclick="fjernTur(${index})">Fjern</button>
+          <button class="bestill" onclick="leggTilTilMineturer(${index})">Bestill tur</button>
+        </div>
+      </div>
+    `;
+
+    lagredeTurerElement.appendChild(turElement);
+  });
+}
+
+
+    function fjernTur(index) {
+      var lagredeTurer = JSON.parse(localStorage.getItem('lagredeTurer')) || [];
+
+      if (confirm('Er du sikker på at du vil fjerne denne turen?')) {
+        lagredeTurer.splice(index, 1);
+        localStorage.setItem('lagredeTurer', JSON.stringify(lagredeTurer));
+
+        var lagredeTurerElement = document.getElementById('lagredeTurer');
+        lagredeTurerElement.innerHTML = ''; // Tømmer beholderen
+
+        // Oppdater visningen ved å legge til de gjenværende turene igjen
+        leggTilLagredeTurer();
+      }
+    }
+
+    // Kjør funksjonen for å legge til lagrede turer når siden lastes
+    leggTilLagredeTurer();
---
+++ mineturer.js
@@ -0,0 +1,27 @@
+window.onload = function() {
+    var turerDiv = document.getElementById('turer-liste');
+    var mineturer = JSON.parse(localStorage.getItem('mineturer')) || [];
+
+    mineturer.forEach(function(tur, index) {
+      var turElement = document.createElement('div');
+      turElement.classList.add('tour');
+      turElement.innerHTML = `
+        <img src="${tur.bildeUrl}" alt="${tur.tittel}">
+        <div class="tour-details">
+          <h2 class="tour-title">${tur.tittel}</h2>
+          <p class="tour-description">${tur.beskrivelse}</p>
+          <button class="remove-btn" data-index="${index}">Fjern</button>
+        </div>
+      `;
+      turerDiv.appendChild(turElement);
+    });
+
+    turerDiv.addEventListener('click', function(event) {
+      if (event.target.classList.contains('remove-btn')) {
+        var indexToRemove = event.target.dataset.index;
+        mineturer.splice(indexToRemove, 1);
+        localStorage.setItem('mineturer', JSON.stringify(mineturer));
+        event.target.closest('.tour').remove(); // Fjerner hele tour-elementet
+      }
+    });
+  };
